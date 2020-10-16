95a6aef9edabb0213a6002af02db1e149ffc2a05
VajihuddinAhmed <vajihahmed@gmail.com>
2020-10-17 00:04:47 +0200

    Home page completed with functionalities

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,21 @@
-import { writeFileSync } from 'fs'; 
-import { finished } from 'stream';
+// let userdata = (key, value) => {
+//     localStorage.setItem(key, value)
+// }
+const formSubmit = document.querySelector("#submit")
 
-let student = {
-    name : 'name',
-    id: 12
-}
+formSubmit.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userName = e.target.elements.name.value.trim()
+    const Id = e.target.elements.Id.value.trim()
+    const email = e.target.elements.email.value.trim()
+    const code = e.target.elements.code.value.trim()
 
-const name = document.querySelector('#name')
+    // userdata('Name:', userName)
 
-name.addEventListener('change', (e) => {
-    let res = e.target.value
-    student.name = res
-})
+    localStorage.setItem('Name:', JSON.stringify(userName))
+    localStorage.setItem('ID:', JSON.stringify(Id))
+    localStorage.setItem('Email:', JSON.stringify(email))
+    localStorage.setItem('Code:', JSON.stringify(code)) 
 
-const id = document.querySelector('#identification')
-
-id.addEventListener('change', (e) => {
-    let id = e.target.value
-    student.id = id
-})
-
-const sub = document.querySelector('#submit')
-
-sub.addEventListener('submit', (e) => {
-    let data = JSON.stringify(student);
-    writeFileSync('file.json', data, finished);
-    console.log('success')
+    location.assign(`/html/tests.html`)
 })
