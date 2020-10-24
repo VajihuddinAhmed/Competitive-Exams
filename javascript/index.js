33de631e24a74510e3fa1dfd258d8bb2e74a4a8f
VajihuddinAhmed <vajihahmed@gmail.com>
2020-10-24 08:01:28 +0200

    footer issues fixed

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,24 @@
+const postUrl = 'https://my-json-server.typicode.com/VajihuddinAhmed/Job_Board_API/postjobs';
+
 document.querySelector("#login").addEventListener('click', () => {
     location.assign(`/html/login.html`)
+})
+
+const subscribe = document.querySelector('#subscribe')
+subscribe.addEventListener('click', () => {
+    const check = document.getElementById('subscribeInput').value 
+    let data = {
+        subscribeEmail: check
+    }
+
+    fetch(postUrl, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    }).then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+
+    document.getElementById('subscribeInput').value = ''
+
 })
